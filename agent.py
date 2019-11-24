05ce8158ab9280c98282f4ed7bd22b071712420f
JUNG Myoungki <myoungki.jung@gmail.com>
2019-11-24 14:59:45 +1000

    Added Agent and Algorithm Description

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ from model import Actor, Critic, OUNoise, ReplayBuffer
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
+TAU = 1e-3              # coefficient for soft update of target
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
@@ -39,24 +39,18 @@ class DDPG_agent():
         
         # Replay memory
         self.memory = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE, random_seed)
-        #self.memory = MemoryBuffer(BUFFER_SIZE, BATCH_SIZE)
         
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
         # convert state from numpy to pytorch array 
         state = torch.from_numpy(state).float().to(device)
-        # use actor_local to predict action
-        # turn nn into evaluation mode 
         self.actor_local.eval()
-        # turn off computing gradients
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
-        # turn the nn into training mode
         self.actor_local.train()
         if add_noise:
             action += self.noise.sample()
         
-        # clipping the action from min to max
         return np.clip(action, -1, 1)
         
     def step(self, state, action, reward, next_state, done):
@@ -92,36 +86,19 @@ class DDPG_agent():
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
-        # Compute Q targets for current states (y_i)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-        # Compute critic loss
         Q_expected = self.critic_local(states, actions)
-        # mean squared error loss
         critic_loss = F.mse_loss(Q_expected, Q_targets)
-        # Minimize the loss
-        # zero_grad because we do not want to accumulate 
-        # gradients from other batches, so needs to be cleared
         self.critic_optimizer.zero_grad()
-        # compute derivatives for all variables that
-        # requires_grad-True
         critic_loss.backward()
-        # update those variables that requires_grad-True
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
         # Compute actor loss
-        # take the current states and predict actions
         actions_pred = self.actor_local(states)
-        # -1 * (maximize) Q value for the current prediction
         actor_loss = -self.critic_local(states, actions_pred).mean()
-        # Minimize the loss
-        # zero_grad because we do not want to accumulate 
-        # gradients from other batches, so needs to be cleared
         self.actor_optimizer.zero_grad()
-        # compute derivatives for all variables that
-        # requires_grad-True
         actor_loss.backward()
-        # update those variables that requires_grad-True
         self.actor_optimizer.step()
 
         # ----------------------- update target networks ----------------------- #
