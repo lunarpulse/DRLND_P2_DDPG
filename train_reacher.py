7a2f1a3c587c742db9df1c140ab5e3b95201e02b
JUNG Myoungki <myoungki.jung@gmail.com>
2020-01-04 23:43:44 +1000

    Refactored

--- train_reacher.py
+++ train_reacher.py
@@ -1,41 +1,91 @@
 from unityagents import UnityEnvironment
-import numpy as np
-
-env = UnityEnvironment(file_name='./Reacher_Linux/Reacher.x86_64')
-
-brain_name = env.brain_names[0]
-brain = env.brains[brain_name]
-
-env_info = env.reset(train_mode=True)[brain_name]
-
-# number of agents
-num_agents = len(env_info.agents)
-print('Number of agents:', num_agents)
-
-# size of each action
-action_size = brain.vector_action_space_size
-print('Size of each action:', action_size)
-
-# examine the state space 
-states = env_info.vector_observations
-state_size = states.shape[1]
-print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
-print('The state for the first agent looks like:', states[0])
 
 from agent import DDPG_agent
 import numpy as np
 import torch
 
 from collections import namedtuple, deque
-
-agents = DDPG_agent(state_size=state_size, action_size=action_size, num_agents=num_agents, random_seed=0)
-n_episodes = 1000
-print_every = 10
-
 import time
-def ddpg(n_episodes=2000, max_t=1000):
+
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def env_parse(env):
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents
+    num_agents = len(env_info.agents)
+    print('Number of agents:', num_agents)
+
+    # size of each action
+    action_size = brain.vector_action_space_size
+    print('Size of each action:', action_size)
+
+    # examine the state space 
+    states = env_info.vector_observations
+    state_size = states.shape[1]
+    print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('The state for the first agent looks like:', states[0])
+
+    return brain, brain_name, state_size, action_size, num_agents
+
+def replay(env, max_timesteps, actor_ckpt, critic_ckpt):
+    agents.actor_local.load_state_dict(torch.load(actor_ckpt, map_location='cpu'))
+    agents.critic_local.load_state_dict(torch.load(critic_ckpt, map_location='cpu'))
+
+    brain_name = env.brain_names[0]
+    env_info = env.reset(train_mode=False)[brain_name]        
+    states = env_info.vector_observations                  
+    scores = np.zeros(num_agents)                          
+
+    for i in range(max_timesteps):
+        actions = agents.act(states, add_noise=False)                    
+        env_info = env.step(actions)[brain_name]        
+        next_states = env_info.vector_observations        
+        rewards = env_info.rewards                        
+        dones = env_info.local_done                 
+        scores += rewards                         
+        states = next_states                              
+        if np.any(dones):                              
+            break
+    print("Scores:", scores)
+
+def plot(score):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(1, len(score)+1), score)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
+
+def plot_adv(fpath):
+    with open(fpath, "rb") as fp:
+        scores = pickle.load(fp)
+
+    episodes = np.arange(len(scores)) + 1
+    scores = pd.Series(scores)
+    scores_ma = scores.rolling(100).mean()
+
+    plt.figure(figsize=[8,4])
+    plt.xlabel("Episode")
+    plt.ylabel("Score")
+    plt.plot(episodes, scores, c="blue")
+    plt.plot(episodes, scores_ma, c="red", linewidth=3)
+    plt.axhline(30, linestyle="--", color="black", linewidth=2)
+    plt.grid(which="major")
+    plt.legend(["Episode Average Score", "Moving Average Score (100 Episodes)",
+                "Criteria"])
+    plt.tight_layout()
+    plt.savefig("./bestmodel_score.png")
+
+def train(env, brain_name, agents, n_episodes=2000, print_every = 10, max_t=1000, target_score = 0, actor_ckpt = 'model_checkpoint_actor.pth', critic_ckpt = 'nodel_checkpoint_critic.pth'):
     scores_deque = deque(maxlen=50)
     scores = []
+    t0=time.time()
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.vector_observations
@@ -55,45 +105,42 @@ def ddpg(n_episodes=2000, max_t=1000):
                 break 
         scores_deque.append(np.mean(score))
         scores.append(np.mean(score))
-        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.3f}'.format(i_episode, 
-                                                                          np.mean(scores_deque), 
-                                                                          np.mean(score)))
+
         average_score = np.mean(scores_deque)
-        if i_episode % print_every == 20 or average_score > 30:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, average_score))
-            torch.save(agents.actor_local.state_dict(), 'reacher_checkpoint_actor.pth')
-            torch.save(agents.critic_local.state_dict(), 'reacher_checkpoint_critic.pth') 
-        if average_score > 30:
+        if i_episode % print_every == 0 or average_score > target_score:
+            print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.3f}'.format(i_episode, average_score, np.mean(score)))
+            torch.save(agents.actor_local.state_dict(), actor_ckpt)
+            torch.save(agents.critic_local.state_dict(), critic_ckpt) 
+        if average_score > target_score:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, average_score))
             break
+    t1=time.time()
+
+    print("\nTotal time elapsed: {} seconds".format(t1-t0))
     return scores
 
-score = ddpg()
 
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(1, len(score)+1), score)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-plt.show()
+if __name__ == "__main__":
+    env = UnityEnvironment(file_name='./Reacher_Linux/Reacher.x86_64')
 
-agents.actor_local.load_state_dict(torch.load('reacher_checkpoint_actor.pth', map_location='cpu'))
-agents.critic_local.load_state_dict(torch.load('reacher_checkpoint_critic.pth', map_location='cpu'))
+    brain, brain_name, state_size, action_size, num_agents = env_parse(env)
 
-env_info = env.reset(train_mode=False)[brain_name]        
-states = env_info.vector_observations                  
-scores = np.zeros(num_agents)                          
+    agents = DDPG_agent(state_size=state_size, action_size=action_size, num_agents=num_agents, random_seed=0)
+    
+    n_episodes = 150
+    print_every = 5
+    max_timesteps = 1000
+    target_score = 30
+    actor_ckpt = 'reacher_checkpoint_actor.pth'
+    critic_ckpt = 'reacher_checkpoint_critic.pth'
 
-for i in range(1000):
-    actions = agents.act(states, add_noise=False)                    
-    env_info = env.step(actions)[brain_name]        
-    next_states = env_info.vector_observations        
-    rewards = env_info.rewards                        
-    dones = env_info.local_done                 
-    scores += rewards                         
-    states = next_states                              
-    if np.any(dones):                              
-        break
-print("Scores:", scores)
-env.close()
+    # Training
+    score_list = train(env, brain_name, agents, n_episodes, print_every, max_timesteps, target_score, actor_ckpt, critic_ckpt)
+
+    # Plot
+    plot(score_list)
+
+    # Replay
+    replay(env, max_timesteps, actor_ckpt, critic_ckpt)
+    
+    env.close()
