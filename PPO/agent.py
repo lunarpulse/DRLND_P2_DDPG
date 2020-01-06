Fix Train msgs and score calculation

PPO/agent.py
@@ -68,46 +68,6 @@ class ProximalPolicyOptimisation:
 
         return actions
         
-    def collect_trajectories(self):
-        buffer = dict([(k, []) for k in self.buffer_attrs])
-
-        for t in range(self.tmax):
-            memory = {}
-
-            # draw action from model
-            memory["states"] = self.last_states
-            pred = self.model(memory["states"])
-            pred = [v.detach() for v in pred]
-            memory["actions"], memory["log_probs"], _, memory["values"] = pred
-
-            # one step forward
-            actions_np = memory["actions"].cpu().numpy()
-            env_info = self.env.step(actions_np)[self.brain_name]
-            memory["next_states"] = self.to_tensor(env_info.vector_observations)
-            memory["rewards"] = self.to_tensor(env_info.rewards)
-            memory["dones"] = self.to_tensor(env_info.local_done, dtype=np.uint8)
-
-            # stack one step memory to buffer
-            for k, v in memory.items():
-                buffer[k].append(v.unsqueeze(0))
-
-            self.last_states = memory["next_states"]
-            r = np.array(env_info.rewards)[None,:]
-            if self.rewards is None:
-                self.rewards = r
-            else:
-                self.rewards = np.r_[self.rewards, r]
-
-            if memory["dones"].any():
-                rewards_mean = self.rewards.sum(axis=0).mean()
-                self.scores_by_episode.append(rewards_mean)
-                self.rewards = None
-
-        for k, v in buffer.items():
-            buffer[k] = torch.cat(v, dim=0)
-
-        return buffer
-
     def calc_returns(self, rewards, values, dones, last_values):
         n_step, n_agent = rewards.shape
 
@@ -144,14 +104,13 @@ class ProximalPolicyOptimisation:
         self.rewards.append(rewards)
         self.next_states.append(next_states)
         self.dones.append(dones)
-        score = rewards.sum(dim=0).mean()
 
         # here to be passed after iteration batch 
         if self.batch_number == self.batch_size - 1 :
             self.batch_number = 0
-            self.model.eval()
+            # self.model.eval()
             # Calculate Score (averaged over agents)
-            score = torch.cat(self.rewards, dim=0).sum(dim=0).mean()
+            # score = torch.cat(self.rewards, dim=0).sum(dim=0).mean()
 
             # Append Values collesponding to last states
             # Get the expected_value
@@ -213,7 +172,6 @@ class ProximalPolicyOptimisation:
                     del(loss)
         else:
             self.batch_number += 1
-        return score
         
     def save(self, filename):
         torch.save(self.model.state_dict(), filename)

train_reacher.py
@@ -41,18 +41,16 @@ def replay(env, agents, max_timesteps, model_ckpt):
 
     brain_name = env.brain_names[0]
     env_info = env.reset(train_mode=False)[brain_name]        
-    states = env_info.vector_observations                  
+    states =  to_tensor(env_info.vector_observations, device = device)                  
     scores = np.zeros(num_agents)                          
 
     for i in range(max_timesteps):
         actions = agents.act(states)                    
-        env_info = env.step(actions)[brain_name]        
-        next_states = env_info.vector_observations        
-        rewards = env_info.rewards                        
-        dones = env_info.local_done                 
+        env_info = env.step(actions.cpu().data.numpy())[brain_name]
+        rewards, next_states, dones = convert(env_info, device)
         scores += rewards                         
         states = next_states                              
-        if np.any(dones):                              
+        if np.any(dones.cpu().data.numpy()):                              
             break
     print("Scores:", scores)
 
@@ -97,7 +95,7 @@ def train(env, agents, n_episodes=2000, print_every = 10, max_t=1000, target_sco
     brain_name = env.brain_names[0]
     env_info = env.reset(train_mode=True)[brain_name]
     num_agents = len(env_info.agents)
-    scores_deque = deque(maxlen=50)
+    scores_deque = deque(maxlen=100)
     scores = []
     t0=time.time()
     for i_episode in range(1, n_episodes+1):
@@ -109,21 +107,20 @@ def train(env, agents, n_episodes=2000, print_every = 10, max_t=1000, target_sco
             actions = agents.act(states)
             env_info = env.step(actions.cpu().data.numpy())[brain_name]
             rewards, next_states, dones = convert(env_info, device)
-            agents.step(states, actions, rewards, next_states, dones)
             states = next_states
+            agents.step(states, actions, rewards, next_states, dones)
             score += rewards
             if np.any(dones.cpu().data.numpy()):
                 break
-            
-        scores_deque.append(np.mean(score))
-        scores.append(np.mean(score))
-
+        scores_deque.append(score.mean())
+        scores.append(score.mean())
         average_score = np.mean(scores_deque)
-        if i_episode % print_every == 0 or average_score > target_score:
-            print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.3f}'.format(i_episode, average_score, np.mean(score)))
-            agents.save(model_ckpt)
+        if i_episode % print_every == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.3f}'.format(i_episode, average_score, score.mean()))
+            
         if average_score > target_score:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, average_score))
+            print('\rTraining Finished at the episode {}\t Average Score: {:.2f}'.format(i_episode, average_score))
+            agents.save(model_ckpt)
             break
     t1=time.time()
 